4c6ad3737b3cbc3d44351f7fbf1f926928f6ef07
Tata-Link <83451272+Tata-git@users.noreply.github.com>
2022-06-03 15:51:51 +0300

    error hbs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import './css/styles.css';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+import './css/styles.css';
+import getRefs from './js/get-refs';
+import { fetchCountries } from './js/fetchCountries';
+// import countryCharacteristics from './templates/countryCharacteristics.hbs'
+// import countryItem from './templates/countryItem.hbs'
+
+const debounce = require('lodash.debounce');
+
+const refs = getRefs(); //searchBox/countryList/countryInfo
 const DEBOUNCE_DELAY = 300;
+// console.log(refs.searchBox);
+let countryName = '';
+
+refs.searchBox.addEventListener(
+  'input',
+  debounce(renderCountryCard, DEBOUNCE_DELAY)
+);
+
+function renderCountryCard(evt) {
+  countryName = evt.target.value.trim();
+  console.log(countryName);
+
+  clearCountryInfo();
+}
+
+function renderCountryInfo(data) {
+  const markup = countryCharacteristics(data);
+  refs.countryInfo.innerHTML = markup;
+  console.log(markup);
+}
+
+function clearCountryInfo() {
+  refs.countryInfo.innerHTML = '';
+}
+//-----------------------------------------
+
